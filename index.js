bee6133d15efbea7bb06ada9d0d20412168b04e9
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2018-10-24 21:11:32 +0100

    Fixed size.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function (e) {
     const area = document.getElementById('area');
-    new generator(20, area.clientWidth, area.clientHeight, 16, 12).createLayout().forEach(e => {
+    new generator(30, area.clientWidth, area.clientHeight, 16, 12).createLayout().forEach(e => {
         area.appendChild(e);
     });
 
