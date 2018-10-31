43482181daf94df974cc372e76a02190dc9fce80
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2018-10-31 13:23:22 +0000

    Added svg dynamical creation

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function (e) {
-    const area = document.getElementById('area');
-    new generator(30, area.clientWidth, area.clientHeight, 16, 12).createLayout().forEach(e => {
-        area.appendChild(e);
+    let aCount = new svg();
+    document.body.appendChild(aCount);
+    new generator(30, aCount.clientWidth, aCount.clientHeight, 16, 12).createLayout().forEach(e => {
+        aCount.appendChild(e);
     });
 
 });
---
+++ shapes/svg.js
@@ -0,0 +1,9 @@
+class svg {
+    constructor() {
+        let e = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        e.setAttribute('width', 2000);
+        e.setAttribute('height', 1071);
+
+        return e;
+    }
+}
