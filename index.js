a30616195cbf2138f714cb41003f39469ad42dee
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2019-02-07 13:40:22 +0000

    Fixed min max.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function (e) {
     let m = new manager();
-    m.getNew(new count(22, 18));
+    m.getNew(new count(18, 22));
     m.getNew(new draw());
     m.getNew(new pattern());
 });
