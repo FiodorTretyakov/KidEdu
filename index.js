cfef03c093a53d1cba10e54dc7c12d00b9edd649
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2019-02-12 10:55:38 +0000

    Changed numbers.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function (e) {
     let m = new manager();
-    m.getNew(new count(25, 30));
+    m.getNew(new count(28, 30));
     m.getNew(new draw());
     
     //disabled for now
