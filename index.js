4665bcd92018c311ebb4260c345a380ba3e44d8a
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2019-02-21 11:46:55 +0000

    Fixed timeline to 30.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     let m = new manager();
-    m.getNew(new count(28, 30));
+    m.getNew(new count(30, 30));
     m.getNew(new draw());
     m.getNew(new countAlive(1, 10));
 
