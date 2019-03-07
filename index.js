e44ac39d64ed6e5e92cbe66989ccbbf8082f9f35
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2019-03-07 16:32:32 +0000

    Added limit to 40.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     let m = new manager();
-    //m.getNew(new count(30, 30));
+    m.getNew(new count(40, 40));
     //m.getNew(new draw());
-    m.getNew(new drawDigits());
+    //m.getNew(new drawDigits());
     //m.getNew(new countAlive(1, 10));
 
     //disabled for now
